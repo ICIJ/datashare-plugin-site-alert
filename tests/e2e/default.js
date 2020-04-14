8e473901cbbdfc49d355f5fb28e5b2b1b32b9e2f
Pierre Romera <hello@pirhoo.com>
2020-04-14 12:28:13 +0200

    Added test

--- tests/e2e/default.js
+++ tests/e2e/default.js
@@ -12,7 +12,8 @@ module.exports = {
     // Move to the search page
     browser.click('.search-bar__submit')
     browser.waitForElementVisible('.search')
-    browser.expect.element('.search .app__nav .site-alert').to.be.present
-    browser.expect.element('.search .app__nav .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
+    browser.expect.element('.search > span .site-alert').to.be.present
+    browser.expect.element('.search > span .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
+    browser.expect.element('.search > span .site-alert').to.have.property('classList').contain('bg-warning')
   }
 };
