32df488c6bbb2e60182c1b5e2561d70f31889bda
Anne L'Hôte <anne.lhote@gmail.com>
2020-08-26 11:03:37 +0200

    Enable site alert only for a specific project

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import Alert from './Alert.vue'
 
 document.addEventListener('datashare:ready', async ({ detail }) => {
-  detail.core.registerHook({ target: 'landing.form:before', definition: Alert })
-  detail.core.registerHook({ target: 'search:before', definition: Alert })
+  const project = detail.core.config.get('appAlertProject', null)
+  if (project) {
+    detail.core.registerHookForProject(project, { target: 'landing.form:before', definition: Alert })
+    detail.core.registerHookForProject(project, { target: 'search:before', definition: Alert })
+  } else {
+    detail.core.registerHook({ target: 'landing.form:before', definition: Alert })
+    detail.core.registerHook({ target: 'search:before', definition: Alert })
+  }
 }, false)
