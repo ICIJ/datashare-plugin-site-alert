38fd0208d8d1b2b0bdeb52b97a96e3929ba07c69
Pierre Romera <hello@pirhoo.com>
2024-06-21 18:07:08 +0000

    ci: bump Datashare to 17.0.4

--- tests/e2e/default.js
+++ tests/e2e/default.js
@@ -9,17 +9,17 @@ module.exports = {
     browser.expect.element(`script[src="/plugins/package/${main}"]`).to.be.present
   },
   // This test is currently disabled
-  'add a site alert': !function(browser) {
+  'add a site alert': function(browser) {
     browser.url('http://localhost:8008')
     // Wait for the app to be started
     browser.waitForElementVisible('.landing')
     browser.expect.element('.landing .site-alert').to.be.present
     browser.expect.element('.landing .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
     // Move to the search page
-    browser.click('.search-bar__submit')
+    browser.click('.search-bar-input__submit')
     browser.waitForElementVisible('.search')
-    browser.expect.element('.search > span .site-alert').to.be.present
-    browser.expect.element('.search > span .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
-    browser.expect.element('.search > span .site-alert').to.have.property('classList').contain('bg-warning')
+    browser.expect.element('.search .site-alert').to.be.present
+    browser.expect.element('.search .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
+    browser.expect.element('.search .site-alert').to.have.property('classList').contain('bg-warning')
   }
 }
