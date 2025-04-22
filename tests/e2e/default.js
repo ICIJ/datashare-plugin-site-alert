319f68addc89a990b6677c57f700f1c44ad25e8c
Pierre Romera <hello@pirhoo.com>
2025-04-22 08:55:04 +0000

    spec: simplify selector

--- tests/e2e/default.js
+++ tests/e2e/default.js
@@ -12,14 +12,8 @@ module.exports = {
   'add a site alert': function(browser) {
     browser.url('http://localhost:8008')
     // Wait for the app to be started
-    browser.waitForElementVisible('.landing')
-    browser.expect.element('.landing .site-alert').to.be.present
-    browser.expect.element('.landing .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
-    // Move to the search page
-    browser.click('.search-bar-input__submit')
-    browser.waitForElementVisible('.search')
-    browser.expect.element('.search .site-alert').to.be.present
-    browser.expect.element('.search .site-alert').text.to.startWith('Hello local, welcome to Datashare!')
-    browser.expect.element('.search .site-alert').to.have.property('classList').contain('bg-warning')
+    browser.waitForElementVisible('.app')
+    browser.expect.element('.site-alert').to.be.present
+    browser.expect.element('.site-alert').text.to.startWith('Hello local, welcome to Datashare!')
   }
 }
